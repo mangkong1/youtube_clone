34f5cb97583862c92e6977fa8cf36e8edbee752b
mangkong1 <xogud5053@hanmail.net>
2024-04-25 15:04:35 +0900

    Refactor: header, aside ts코드 추가

--- src/features/main/views/Main.tsx
+++ src/features/main/views/Main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import MainElem from "./MainElem";
 import Data from "../../../data.tsx";
-import { MainContainer } from "../styles/MainStyle.jsx";
+import { MainContainer } from "../styles/MainStyle.tsx";
 import type { DataType } from "../../../data.tsx"; // Import the DataType interface
 
-const Main: React.FC = () => {
+function Main() {
   const sortedData = [...Data].sort((a: DataType, b: DataType) => new Date(b.uploadDate).getTime() - new Date(a.uploadDate).getTime()); // date 기준으로 오름차순 정렬
 
   return (
@@ -14,6 +14,6 @@ const Main: React.FC = () => {
       ))}
     </MainContainer>
   );
-};
+}
 
 export default Main;
--- src/features/main/views/MainElem.tsx
+++ src/features/main/views/MainElem.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import * as S from "../styles/MainStyle";
+import * as S from "../styles/MainStyle.tsx";
 import { Link } from "react-router-dom";
 import DotIcon from "../../../assets/images/dot.png";
 import useWeeksAgo from "../../../hooks/MainUploadDateHook";
